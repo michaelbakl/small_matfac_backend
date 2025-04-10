a36e03ca6d13bd4d7a5620107455e5368eb6d568
michaelbakl <baklykovmichael@gmail.com>
2025-04-10 18:43:16 +0600

    [*] updated game model class

--- src/main/kotlin/app/core/model/game/Game.kt
+++ src/main/kotlin/app/core/model/game/Game.kt
@@ -1,21 +1,17 @@
 package ru.baklykov.app.core.model.game
 
-import ru.baklykov.app.core.model.Question
+import ru.baklykov.app.core.model.question.Question
 import ru.baklykov.app.core.model.StudentAnswer
 import ru.baklykov.app.core.model.StudentResult
-import java.time.LocalDateTime
-import java.time.ZonedDateTime
 import java.util.*
 
 data class Game(
     val gameId: UUID,
     val roomId: UUID,
-    val name: String,
+    val creatorId: UUID,
     val questions: List<Question>,
     val config: GameConfig,
     val status: String,
-    val startDate: ZonedDateTime,
-    val finishDate: ZonedDateTime? = startDate.plusMinutes(60),
     val studentsAnswers: List<StudentAnswer>? = listOf(),
     val studentsResults: List<StudentResult>? = listOf(),
 )
