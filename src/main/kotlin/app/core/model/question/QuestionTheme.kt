a9ff030710f3f0ded871bb3df43debe41af1cee4
michaelbakl <baklykovmichael@gmail.com>
2025-04-10 18:35:10 +0600

    [+] added QuestionsThemes class

---
+++ src/main/kotlin/app/core/model/question/QuestionTheme.kt
@@ -0,0 +1,15 @@
+package ru.baklykov.app.core.model.question
+
+import java.util.*
+
+data class QuestionTheme(
+    val id: UUID,
+    val path: String, // LTREE в виде строки
+    val name: String,
+    val level: Int
+) {
+
+    fun isChildOf(parentPath: String): Boolean {
+        return path.startsWith("$parentPath.")
+    }
+}
