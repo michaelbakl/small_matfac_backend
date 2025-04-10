ee9fa9c9c9abd17733e9d2691c56d443869a5a16
michaelbakl <baklykovmichael@gmail.com>
2025-04-10 18:43:39 +0600

    [*] updated game config model class

--- src/main/kotlin/app/core/model/game/GameConfig.kt
+++ src/main/kotlin/app/core/model/game/GameConfig.kt
@@ -6,11 +6,13 @@ import java.util.*
 data class GameConfig(
     val categories: List<UUID> ?= listOf(),
     val questionCount: Int ?= 30,
+    val name: String ?= "Game",
     val duration: Int ?= 30,
     val gameType: GameType ?= GameType.SINGLE,
     val difficulty: DifficultyLevel ?= DifficultyLevel.MEDIUM,
     val allowSkips: Boolean = false,
     val enableHints: Boolean = false,
     val questions: List<UUID> ?= listOf(),
-    val startDate: ZonedDateTime? = ZonedDateTime.now()
+    val startDate: ZonedDateTime? = ZonedDateTime.now(),
+    val finishDate: ZonedDateTime? = startDate?.plusMinutes(60),
 )
