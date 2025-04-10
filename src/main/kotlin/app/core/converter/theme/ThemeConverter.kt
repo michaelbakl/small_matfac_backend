e9d4b98a87b6a4e58874534917ad36800925e358
michaelbakl <baklykovmichael@gmail.com>
2025-04-10 18:32:59 +0600

    [+] added new converter for question themes

---
+++ src/main/kotlin/app/core/converter/theme/ThemeConverter.kt
@@ -0,0 +1,14 @@
+package ru.baklykov.app.core.converter.theme
+
+import app.core.converter.IConverter
+import ru.baklykov.app.core.model.question.QuestionTheme
+import ru.baklykov.app.web.model.response.theme.ThemeResponse
+
+object ThemeConverter: IConverter<ThemeResponse, QuestionTheme> {
+    override fun convert(obj: QuestionTheme): ThemeResponse {
+        return ThemeResponse(
+            obj.id, obj.name, obj.path, obj.level, hasChildren = true,
+            parentId = TODO()
+        )
+    }
+}
